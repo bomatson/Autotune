955c23699596df048d8205cf3ccb0f263a05b262
Bobby Matson <bobby.matson@gmail.com>
2012-06-06 14:03:10 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Autotune: Music suggestions that matter
+
+Built for the Hollywood HackDay 2012
+
+[*Learn About Me*](http://about.me/bobbymatson)
+
+[*@bomaton*](https://twitter.com/bomatson)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autotune::Application.config.secret_token = '4cd589e348d0eef9d4a0b639552051d6040103ec463197180df5abd6716a9b2e5b189f23b382021847a48d7f81e920f0c4f348fb1b9c0813ed83454b0aaa1e6f'
