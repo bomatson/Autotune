bdf4cf1d6bd8e294e0d9b411b2e5189368c58928
Bobby Matson <bobby.matson@gmail.com>
2012-06-18 17:16:25 -0700

    adjust rdio session

--- lib/rdio/session.rb
+++ lib/rdio/session.rb
@@ -4,16 +4,16 @@ require 'omniauth-rdio'
 module Rdio
   class Session
     
-    def initialize(session = nil)#, username)
-      raise NoUserSession if session.nil?
-      @session = session
-    end
-
-  private
-    def create_rdio_connection
-      Rdio::init(ENV['RDIO_KEY'], ENV['RDIO_SECRET']).tap do |rdio|
-        rdio.session = @session
-      end      
-    end    
+   #  def initialize(session = nil)#, username)
+   #     raise NoUserSession if session.nil?
+   #     @session = session
+   #   end
+   # 
+   # private
+   #   def create_rdio_connection
+   #     Rdio::init(ENV['RDIO_KEY'], ENV['RDIO_SECRET']).tap do |rdio|
+   #       rdio.session = @session
+   #     end      
+   #   end    
   end
 end
